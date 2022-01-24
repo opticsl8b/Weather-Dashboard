0090ceda1e433f81b55700dde69fd8bd5db10895
George Cheng <sparklyspuds@gmail.com>
2022-01-24 23:39:33 +0800

    HTML:100%, CSS:100% , JS:98%, Readme: 100%

--- README.md
+++ README.md
@@ -1,2 +1,102 @@
-# weather-dashboard
-A dashboard allow users have a weather outlook for multiple cities
+## Description
+
+<hr>
+<hr>
+
+<br>
+<h3 align="center">--  Weather Dashboard --</h3>
+<br>
+
+<p align="center">
+<img src="https://i.imgur.com/KRO8Qlc.png" title="source: imgur.com" alt="Weather Dashboard Preview"/>
+</p>
+
+<br>
+<p align="center">
+A simple weather app for weather outlook by cities</p>
+<br>
+
+<hr>
+<hr>
+
+<h2>Funtions</h2>
+<br>
+
+<h3 align="center">- Search & Display I-</h3>
+<br>
+
+<p align="center">
+<img src="https://i.imgur.com/m0TksFd.gif" title="source: imgur.com" alt="Search & Display"/>
+</p>
+<br>
+<p align="center">
+When search for a city, display current and future conditions for that city and that city is added to the search history</p>
+<br>
+
+<hr>
+<hr>
+
+<br>
+<h3 align="center">- Search & Display II -</h3>
+<br>
+
+<p align="center">
+<img src="https://i.imgur.com/hj49YHP.gif" title="source: imgur.com" alt="Search & Display II"/>
+</p>
+<br>
+<p align="center">
+Present with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index </p>
+<br>
+<p align="center">
+Present UV index with a color that indicates whether the conditions are favorable:green, moderate:yellow, or severe:red</p>
+<br>
+<hr>
+<hr>
+
+<br>
+<h3 align="center">- Forecast -</h3>
+<br>
+<p align="center">
+<img src="https://i.imgur.com/UjtWDP1.gif" title="source: imgur.com" alt="Forecast"/>
+</p>
+<br>
+<p align="center">
+Present a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity when view future weather conditions for that city</p>
+<br>
+
+<hr>
+<hr>
+
+<br>
+<h3 align="center">- Search History -</h3>
+<br>
+<p align="center">
+<img src="https://i.imgur.com/GnFxGpA.gif" title="source: imgur.com" alt="Search History"/>
+</p>
+<br>
+
+<p align="center">Presented again with current and future conditions for that city when click on a city in the search history </p>
+<br>
+
+<p align="center">Clear search history area when click on clear button</p>
+<br>
+
+<hr>
+<hr>
+
+<h2>Future scope</h2>
+
+- Display current time with city searched
+
+- Adding responsive viewport for small screen
+
+- Some minor bugs fix
+
+<hr>
+<hr>
+
+<h3 align="center">Link to the page</h3>
+<br>
+
+https://opticsl8b.github.io/Weather-Dashboard/
+
--- script.js
+++ script.js
@@ -4,7 +4,6 @@ var resultCity = document.getElementById('result-city');
 var submitButton = document.getElementById('btn-submit');
 var clearButton = document.getElementById('btn-clear');
 var searchHistory = document.getElementById('search-history');
-// var searchBoxEl = document.getElementById('search-box');
 
 // element selector - Current Box
 var currentDate = document.getElementById('span-date');
@@ -13,16 +12,12 @@ var currentTemp = document.getElementById('span-current-temp');
 var currentWind = document.getElementById('span-current-wind');
 var currentHumidity = document.getElementById('span-current-humidity');
 var currentUV = document.getElementById('span-current-uv');
+
 // element selector - Forecast Box
 var forecastWeather = document.getElementById('weather-cards');
 
 // api key
 var apiKey = "9fd3d63ef022ae3df21d9d5a84d29881"
-
-// Save fetched data into local storage
-var jsonStringify = function(data) {
-    localStorage.setItem('weather', JSON.stringify(data))
-};
 
 // function fetches Geographical coordinates data
 function lonlatFetch() {
@@ -48,19 +43,21 @@ var cardsGenerator = function() {
     var ul = document.createElement('ul');
     lastDiv.appendChild(ul);
 };
+
+// function generates corresponding button-
 // create an array that stores valid city input
 var historyCity = [];
 
-// function generates corresponding button
 function createBtn() {
+
     var createButton = document.createElement('input');
     createButton.setAttribute('class', 'button-primary');
     createButton.setAttribute('type', 'button');
     createButton.setAttribute('value', inputCity.value);
     searchHistory.appendChild(createButton);
-
+    // push city input into the array
     historyCity.push(inputCity.value);
-    console.log(historyCity);
+
     localStorage.setItem('History-Btn', JSON.stringify(historyCity));
 
     //reset input text
@@ -70,7 +67,6 @@ function createBtn() {
 // function clears search history button 
 function clearBtn() {
     searchHistory.innerHTML = '';
-
     localStorage.removeItem('History-Btn');
 };
 
@@ -305,127 +301,110 @@ function initialPage() {
 
 initialPage();
 
-// btnHistory redirect
+// History Button click event
 
 var jqsearchHistory = $("#search-history")
 
 $(jqsearchHistory).on("click", ".button-primary", function(event) {
-        var jqButton = $(event.target);
+    var jqButton = $(event.target);
 
-        var url = `https://api.openweathermap.org/data/2.5/weather?q=${jqButton.val()}&appid=${apiKey}`;
-        fetch(url)
-            .then(response => response.json())
-            .then(function(data) {
-                //getApi from one call
-                var lat = data['coord']['lat'];
-                var lon = data['coord']['lon'];
-                var onecallUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`
-                fetch(onecallUrl)
-                    .then(response => response.json())
-                    .then(function(onecallData) {
-                        if (onecallData) {
+    var url = `https://api.openweathermap.org/data/2.5/weather?q=${jqButton.val()}&appid=${apiKey}`;
+    fetch(url)
+        .then(response => response.json())
+        .then(function(data) {
+            //getApi from one call
+            var lat = data['coord']['lat'];
+            var lon = data['coord']['lon'];
+            var onecallUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`
+            fetch(onecallUrl)
+                .then(response => response.json())
+                .then(function(onecallData) {
+                    if (onecallData) {
 
-                            // display corresponding result-
+                        // display corresponding result-
 
-                            // city name display
-                            resultCity.innerHTML = onecallData['timezone'];
-                            // Unix convert to display format
-                            var dateString = moment.unix(onecallData['current']['dt']).format("DD/MM/YYYY");
-                            // current date
-                            currentDate.innerHTML = dateString;
-                            // weather Icon convert and display
-                            var weatherCode = onecallData['current']['weather'][0]['icon'];
-                            var weatherUrl = `http://openweathermap.org/img/wn/${weatherCode}@2x.png`;
-                            currentWeather.setAttribute("src", weatherUrl);
-                            // temperture display ,only display one digit after integer
-                            currentTemp.innerHTML = kelvinToCelcius(onecallData['current']['temp']).toFixed(1) + '°C';
-                            // wind speed display
-                            currentWind.innerHTML = onecallData['current']['wind_speed'] + ' m/sec';
-                            //Humidity display
-                            currentHumidity.innerHTML = onecallData['current']['humidity'] + ' %';
-                            //UV display -
-                            var uv = onecallData['current']['uvi'];
-                            currentUV.innerHTML = uv;
-                            if (uv < 4) {
-                                currentUV.setAttribute('class', 'green')
-                            }
-                            if (uv >= 4 && uv <= 8) {
-                                currentUV.setAttribute("class", "yellow");
-                            }
-                            if (uv > 9) {
-                                currentUV.setAttribute("class", "red");
-                            }
-
-                            // 5 day forecast-
-                            // Scrape a 5 Days forecast array 
-                            var next5Day = onecallData['daily'].slice(1, 6);
-                            console.log('data is ', next5Day);
-                            // reset forcast 
-                            forecastWeather.innerHTML = '';
-
-                            for (i = 0; i < next5Day.length; i++) {
-                                // create div + ul
-                                cardsGenerator();
-
-                                // select the ul just created
-                                var lastCard = forecastWeather.children[i];
-
-                                // forecast display-Date
-                                var forecastDate = moment.unix(next5Day[i]['dt']).format("DD/MM/YYYY");
-                                lastCard.children[0].innerHTML = "<li><span>" + forecastDate + "</span></li>";
-
-                                // forecast Weather Icon
-                                // scrape icon code
-                                var forcastWeatherCode = next5Day[i]['weather'][0]['icon'];
-                                var forecastIconUrl = `http://openweathermap.org/img/wn/${forcastWeatherCode}@2x.png`;
-                                // create li for icon
-                                var li = document.createElement('li');
-                                lastCard.children[0].appendChild(li);
-                                var forcastWeather = lastCard.children[0].children[1];
-                                // insert img
-                                var img = document.createElement('img');
-                                forcastWeather.appendChild(img);
-                                forcastWeather.children[0].setAttribute("src", forecastIconUrl);
-                                // forecast Temperture
-                                var li = document.createElement('li');
-                                lastCard.children[0].appendChild(li);
-                                var forcastTemp = lastCard.children[0].children[2];
-                                forcastTemp.innerHTML =
-                                    'Temp:<br>' + kelvinToCelcius(next5Day[i]['temp']['min']).toFixed(1) + ' ~ ' +
-                                    kelvinToCelcius(next5Day[i]['temp']['max']).toFixed(1) + ' °C';
-                                // forecast Wind Speed
-                                var li = document.createElement('li');
-                                lastCard.children[0].appendChild(li);
-                                var forcastWind = lastCard.children[0].children[3];
-                                forcastWind.innerHTML = 'WindSpeed:<br>' + next5Day[i]['wind_speed'] + ' m/sec';
-                                // forecast Humidity
-                                var li = document.createElement('li');
-                                lastCard.children[0].appendChild(li);
-                                var forcastHumi = lastCard.children[0].children[4];
-                                forcastHumi.innerHTML = 'Humidity:<br>' + next5Day[i]['humidity'] + ' %'
-                            }
-
+                        // city name display
+                        resultCity.innerHTML = onecallData['timezone'];
+                        // Unix convert to display format
+                        var dateString = moment.unix(onecallData['current']['dt']).format("DD/MM/YYYY");
+                        // current date
+                        currentDate.innerHTML = dateString;
+                        // weather Icon convert and display
+                        var weatherCode = onecallData['current']['weather'][0]['icon'];
+                        var weatherUrl = `http://openweathermap.org/img/wn/${weatherCode}@2x.png`;
+                        currentWeather.setAttribute("src", weatherUrl);
+                        // temperture display ,only display one digit after integer
+                        currentTemp.innerHTML = kelvinToCelcius(onecallData['current']['temp']).toFixed(1) + '°C';
+                        // wind speed display
+                        currentWind.innerHTML = onecallData['current']['wind_speed'] + ' m/sec';
+                        //Humidity display
+                        currentHumidity.innerHTML = onecallData['current']['humidity'] + ' %';
+                        //UV display -
+                        var uv = onecallData['current']['uvi'];
+                        currentUV.innerHTML = uv;
+                        if (uv < 4) {
+                            currentUV.setAttribute('class', 'green')
                         }
-                    })
+                        if (uv >= 4 && uv <= 8) {
+                            currentUV.setAttribute("class", "yellow");
+                        }
+                        if (uv > 9) {
+                            currentUV.setAttribute("class", "red");
+                        }
 
+                        // 5 day forecast-
+                        // Scrape a 5 Days forecast array 
+                        var next5Day = onecallData['daily'].slice(1, 6);
+                        console.log('data is ', next5Day);
+                        // reset forcast 
+                        forecastWeather.innerHTML = '';
 
-            })
-    })
-    // var btnHistory = searchHistory.querySelectorAll('input');
+                        for (i = 0; i < next5Day.length; i++) {
+                            // create div + ul
+                            cardsGenerator();
 
-// btnHistory.addEventListener("click",function(){
-//     console.log(btnHistory);
-// });
+                            // select the ul just created
+                            var lastCard = forecastWeather.children[i];
 
+                            // forecast display-Date
+                            var forecastDate = moment.unix(next5Day[i]['dt']).format("DD/MM/YYYY");
+                            lastCard.children[0].innerHTML = "<li><span>" + forecastDate + "</span></li>";
 
+                            // forecast Weather Icon
+                            // scrape icon code
+                            var forcastWeatherCode = next5Day[i]['weather'][0]['icon'];
+                            var forecastIconUrl = `http://openweathermap.org/img/wn/${forcastWeatherCode}@2x.png`;
+                            // create li for icon
+                            var li = document.createElement('li');
+                            lastCard.children[0].appendChild(li);
+                            var forcastWeather = lastCard.children[0].children[1];
+                            // insert img
+                            var img = document.createElement('img');
+                            forcastWeather.appendChild(img);
+                            forcastWeather.children[0].setAttribute("src", forecastIconUrl);
+                            // forecast Temperture
+                            var li = document.createElement('li');
+                            lastCard.children[0].appendChild(li);
+                            var forcastTemp = lastCard.children[0].children[2];
+                            forcastTemp.innerHTML =
+                                'Temp:<br>' + kelvinToCelcius(next5Day[i]['temp']['min']).toFixed(1) + ' ~ ' +
+                                kelvinToCelcius(next5Day[i]['temp']['max']).toFixed(1) + ' °C';
+                            // forecast Wind Speed
+                            var li = document.createElement('li');
+                            lastCard.children[0].appendChild(li);
+                            var forcastWind = lastCard.children[0].children[3];
+                            forcastWind.innerHTML = 'WindSpeed:<br>' + next5Day[i]['wind_speed'] + ' m/sec';
+                            // forecast Humidity
+                            var li = document.createElement('li');
+                            lastCard.children[0].appendChild(li);
+                            var forcastHumi = lastCard.children[0].children[4];
+                            forcastHumi.innerHTML = 'Humidity:<br>' + next5Day[i]['humidity'] + ' %'
+                        }
 
+                    }
+                })
+        })
+})
 
 submitButton.addEventListener("click", submitButtonHandler);
 clearButton.addEventListener("click", clearBtn);
-
-
-// Search History button event
-// add corresponding(data['name']) button when click on the button-
-
-
-// add button that removes all button
